6977f3d4716f8d9c27d89e2a86515aa6e596cff1
G10dero <202311695@fit.edu.ph>
2025-06-24 15:36:43 +0800

    login page ulet

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../includes/db_connect.php';
-require_once '../includes/session_manager.php';
+require_once 'includes/db_connect.php';
+require_once 'includes/session_manager.php';
 
 $error = "";
 
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="css/login.css">
     <title>Login Page</title>
 </head>
 
